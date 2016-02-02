40a62b6262f9970e417a5433ecc4a8f3f7546fc4
Patrick <tundemoran@gmail.com>
2016-02-01 19:01:46 -0500

    changed name of js draw function to better reflect role init'd grey rectangle for start screen

--- ttb.js
+++ ttb.js
@@ -1,5 +1,7 @@
-function drawCanvas()
+function drawStartCanvas()
 {
 	var canvas = document.getElementById('game-screen');
 	var context = canvas.getContext('2d');
+	context.fillStyle = "rgb(100, 100, 100)";
+    context.fillRect (100, 100, 200, 400);
 }
