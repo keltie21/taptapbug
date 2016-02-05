f55b12b200a627074aa940c1b27f55b08ca14622
Patrick <tundemoran@gmail.com>
2016-02-05 15:54:55 -0500

    added context argument to makeBug. colour now works (orange, black and green bugs)

--- js/ttb.js
+++ js/ttb.js
@@ -2,6 +2,7 @@ function drawStartCanvas()
 {
 	var canvas = document.getElementById('game-screen');
 	var context = canvas.getContext('2d');
-	context.fillStyle = "rgb(100, 100, 100)";
-    context.fillRect (100, 100, 200, 400);
+    makeBug(context, 50, 50, "orange");
+    makeBug(context, 150, 50, "black");
+    makeBug(context, 250, 50, "green");
 }
