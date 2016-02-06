2e77385255f51d34c6c0280d0fc1534c3d988f3e
Patrick <tundemoran@gmail.com>
2016-02-06 11:45:36 -0500

    working on rotate, gonna change how bug is drawn (relative)

--- js/bug.js
+++ js/bug.js
@@ -1,11 +1,32 @@
 // Painting one bug with x, y being left top corner
 function makeBug( context, x, y, color, angle){
- 
+    
     alpha = ".5"; 
-    //http://www.w3schools.com/tags/canvas_globalalpha.asp
     context.globalAlpha = alpha;
+    
+    //global(ish), pulled from bug drawing sequence
+    bugWidth = 10;
+    bugHeight = 40;
 
-      
+    // angle the bug (facing proper direction)
+    radians = ((angle * Math.PI) / 180);
+    
+    context.save();
+    
+    context.translate( x , y); // center on bug
+    console.log(context);
+    context.rotate(radians); // rotate context
+    
+    //context.fillStyle = 'blue';
+    //context.fillRect(25, 25, 50, 50);
+    //context.restore();
+    // console.log(context);
+    // console.log(x);
+    // console.log(y);
+    // console.log(color);
+
+    context.fillStyle="blue";
+    context.fillRect(x,y,10,40);
     /*-- Whiskers, legs and arms--*/
     context.beginPath();
     context.moveTo(x,y);
@@ -21,8 +42,9 @@ function makeBug( context, x, y, color, angle){
     context.lineTo(x, y+40);
     context.lineWidth = 2;
     context.strokeStyle = color;
+    context.stroke(); //na
 
-    /*-- Triangles on the tips --*/
+    /*-- Triangles on the tips --*
     context.moveTo(x,y);
     context.lineTo(x, y+3);
     context.lineTo(x+1.73, y+2.4);
@@ -49,7 +71,7 @@ function makeBug( context, x, y, color, angle){
     context.lineTo(x+10, y+40);
     context.stroke();
 
-    /*-- Body parts --*/     
+    /*-- Body parts --*    
     context.beginPath();
     context.arc(x+5, y+15, 5, 0, 2*Math.PI);
     context.moveTo(x+5, y+21);
@@ -62,7 +84,7 @@ function makeBug( context, x, y, color, angle){
     context.stroke();
     context.fill();
 
-    /*-- Eyes and Mouth --*/
+    /*-- Eyes and Mouth --*
     context.beginPath();
     context.arc(x+3.3, y+13.2, 1, 0, 2*Math.PI);
     context.arc(x+6.75, y+13.2, 1, 0, 2*Math.PI);
@@ -71,4 +93,8 @@ function makeBug( context, x, y, color, angle){
     context.beginPath();
     context.arc(x+5, y+15, 2.5, 0, Math.PI, false);
     context.stroke();
+    */
+    // restore angle
+    context.restore();
+    
 }
--- js/ttb.js
+++ js/ttb.js
@@ -2,7 +2,28 @@ function drawStartCanvas()
 {
 	var canvas = document.getElementById('game-screen');
 	var context = canvas.getContext('2d');
-    makeBug(context, 50, 50, "orange");
-    makeBug(context, 150, 50, "black");
-    makeBug(context, 250, 50, "green");
+	_grid(context);
+    makeBug(context, 50, 50, "orange", 300);
+    makeBug(context, 50, 50, "black", 0);
+    //makeBug(context, 150, 50, "black");
+    //makeBug(context, 250, 50, "green");
+}
+
+//overlays a grid on the screen, for debug purposes
+function _grid(context)
+{
+	for (i = 0; i < 600; i = i + 10)
+	{
+		context.beginPath();
+		context.moveTo(0, i);
+		context.lineTo(400, i);
+		context.stroke();
+	}
+	for (i = 0; i < 400; i = i + 10)
+	{
+		context.beginPath();
+		context.moveTo(i, 0);
+		context.lineTo(i, 600);
+		context.stroke();
+	}
 }
