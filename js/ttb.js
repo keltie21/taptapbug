03acbe16fe374c18063c603336dde7d692c0a9b6
SeanWiz <triclopszx@gmail.com>
2016-02-06 12:59:08 -0500

    Randomised food

--- js/ttb.js
+++ js/ttb.js
@@ -5,5 +5,15 @@ function drawStartCanvas()
     makeBug(context, 50, 50, "orange");
     makeBug(context, 150, 50, "black");
     makeBug(context, 250, 50, "red");
-    addFood(context, 100, 100);
+    //addFood(context, 100, 100);
+    spawnFood(canvas, context);
+}
+
+function spawnFood(canvas, context) {
+    for (i = 0; i < 5; i++) {
+        var x = Math.floor(Math.random() * (canvas.width - 100) + 50);
+        var y = Math.floor(Math.random() * (canvas.height - 200) + 150);
+        addFood(context, x, y);
+    }
+    
 }
