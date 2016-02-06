cda1558300b423552d1d401e5866d05c5a549d06
SeanWiz <triclopszx@gmail.com>
2016-02-06 11:59:21 -0500

    Add food image
    We can upload images to the canvas now

--- js/ttb.js
+++ js/ttb.js
@@ -4,5 +4,6 @@ function drawStartCanvas()
 	var context = canvas.getContext('2d');
     makeBug(context, 50, 50, "orange");
     makeBug(context, 150, 50, "black");
-    makeBug(context, 250, 50, "green");
+    makeBug(context, 250, 50, "red");
+    addFood(context, 100, 100);
 }
