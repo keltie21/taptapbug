f3ce3e6548d9f371caf190cdab5fb50839b5af80
Patrick <tundemoran@gmail.com>
2016-02-07 11:56:38 -0500

    classed bug and food

--- js/bug.js
+++ js/bug.js
@@ -1,103 +1,112 @@
-// Painting one bug with x, y being left top corner
-function makeBug( context, x, y, color, angle){
-    
-    alpha = "1.0"; 
-    context.globalAlpha = alpha;
-    
-    //global(ish), pulled from bug drawing sequence
-    bugWidth = 10;
-    bugHeight = 40;
+// bug class
+var Bug = function (x,y,colour, direction)
+{
+    this.x = x;
+    this.y = y;
+    this.colour = colour;
+    this.direction = direction
 
-    // angle the bug (facing proper direction)
-    radians = ((angle * Math.PI) / 180);
+    // Painting one bug with x, y being left top corner
+    this.makeBug = function ( context, x, y, color, angle){
     
-    // save context pre-rotate
-    context.save();
+        alpha = "1.0"; 
+        context.globalAlpha = alpha;
     
-    // center on bug's center point (so rotate is clean)
-    context.translate( x + (bugWidth/2), y +(bugHeight/2)); 
+        //global(ish), pulled from bug drawing sequence
+        bugWidth = 10;
+        bugHeight = 40;
+
+        // angle the bug (facing proper direction)
+        radians = ((angle * Math.PI) / 180);
     
-    //console.log(context);
-
-    // rotate context
-    context.rotate(radians); 
-
-    // move context back to top left to draw the bug
-    context.translate( bugWidth / -2, bugHeight /-2);
+        // save context pre-rotate
+        context.save();
     
-    //rectangle helpful for positioning
-    //context.fillStyle="blue";
-    //context.fillRect(0,0,10,40);
+        // center on bug's center point (so rotate is clean)
+        context.translate( x + (bugWidth/2), y +(bugHeight/2)); 
+        
+        //console.log(context);
 
-    /*-- Whiskers, legs and arms--*/
-    context.beginPath();
-    context.moveTo(0,0);
-    context.lineTo(5, 15);
-    context.lineTo(10, 0);
-    context.moveTo(5, 20);
-    context.lineTo(4, 22);
-    context.lineTo(6, 22);
-    context.lineTo(5, 20);
-    context.moveTo(0, 20);
-    context.lineTo(10, 40);
-    context.moveTo(10, 20);
-    context.lineTo(0, 40);
-    context.lineWidth = 2;
-    context.strokeStyle = color;
-    context.stroke(); //na
+        // rotate context
+        context.rotate(radians); 
 
-    /*-- Triangles on the tips --*/
-    context.moveTo(0,0);
-    context.lineTo(0, 3);
-    context.lineTo(1.73, 2.4);
-    context.lineTo(0, 0);
-    context.moveTo(10, 0);
-    context.lineTo(8.27, 2.4);
-    context.lineTo(10, 3);
-    context.lineTo(10, 0);
-    context.moveTo(0, 20);
-    context.lineTo(0, 22);
-    context.lineTo(1.6, 21.25);
-    context.lineTo(0, 22);
-    context.moveTo(10, 20);
-    context.lineTo(8.4, 21.25);
-    context.lineTo(10, 22);
-    context.lineTo(10, 20);
-    context.moveTo(0, 40);
-    context.lineTo(0, 38);
-    context.lineTo(1.6, 38.25);
-    context.lineTo(0, 38);
-    context.moveTo(10, 40);
-    context.lineTo(8.4, 38.25);
-    context.lineTo(10, 38);
-    context.lineTo(10, 40);
-    context.stroke();
+        // move context back to top left to draw the bug
+        context.translate( bugWidth / -2, bugHeight /-2);
+        
+        //rectangle helpful for positioning
+        //context.fillStyle="blue";
+        //context.fillRect(0,0,10,40);
 
-    /*-- Body parts --*/    
-    context.beginPath();
-    context.arc(5, 15, 5, 0, 2*Math.PI);
-    context.moveTo(5, 21);
-    context.bezierCurveTo(0, 20, 0, 30, 5, 38.75);
-    context.moveTo(5, 21);	
-    context.bezierCurveTo(10, 20, 10, 30, 5, 38.75);
-    context.fillStyle = color;
-    context.lineWidth = 1;
-    context.strokeStyle = "#000000"
-    context.stroke();
-    context.fill();
+        /*-- Whiskers, legs and arms--*/
+        context.beginPath();
+        context.moveTo(0,0);
+        context.lineTo(5, 15);
+        context.lineTo(10, 0);
+        context.moveTo(5, 20);
+        context.lineTo(4, 22);
+        context.lineTo(6, 22);
+        context.lineTo(5, 20);
+        context.moveTo(0, 20);
+        context.lineTo(10, 40);
+        context.moveTo(10, 20);
+        context.lineTo(0, 40);
+        context.lineWidth = 2;
+        context.strokeStyle = color;
+        context.stroke(); //na
 
-    /*-- Eyes and Mouth --*/
-    context.beginPath();
-    context.arc(3.3, 13.2, 1, 0, 2*Math.PI);
-    context.arc(6.75, 13.2, 1, 0, 2*Math.PI);
-    context.fillStyle = "white";
-    context.fill();
-    context.beginPath();
-    context.arc(5, 15, 2.5, 0, Math.PI, false);
-    context.strokeStyle = "white";
-    context.stroke();
-    
-    // restore context angle
-    context.restore();
-    
+        /*-- Triangles on the tips --*/
+        context.moveTo(0,0);
+        context.lineTo(0, 3);
+        context.lineTo(1.73, 2.4);
+        context.lineTo(0, 0);
+        context.moveTo(10, 0);
+        context.lineTo(8.27, 2.4);
+        context.lineTo(10, 3);
+        context.lineTo(10, 0);
+        context.moveTo(0, 20);
+        context.lineTo(0, 22);
+        context.lineTo(1.6, 21.25);
+        context.lineTo(0, 22);
+        context.moveTo(10, 20);
+        context.lineTo(8.4, 21.25);
+        context.lineTo(10, 22);
+        context.lineTo(10, 20);
+        context.moveTo(0, 40);
+        context.lineTo(0, 38);
+        context.lineTo(1.6, 38.25);
+        context.lineTo(0, 38);
+        context.moveTo(10, 40);
+        context.lineTo(8.4, 38.25);
+        context.lineTo(10, 38);
+        context.lineTo(10, 40);
+        context.stroke();
+
+        /*-- Body parts --*/    
+        context.beginPath();
+        context.arc(5, 15, 5, 0, 2*Math.PI);
+        context.moveTo(5, 21);
+        context.bezierCurveTo(0, 20, 0, 30, 5, 38.75);
+        context.moveTo(5, 21);	
+        context.bezierCurveTo(10, 20, 10, 30, 5, 38.75);
+        context.fillStyle = color;
+        context.lineWidth = 1;
+        context.strokeStyle = "#000000"
+        context.stroke();
+        context.fill();
+
+        /*-- Eyes and Mouth --*/
+        context.beginPath();
+        context.arc(3.3, 13.2, 1, 0, 2*Math.PI);
+        context.arc(6.75, 13.2, 1, 0, 2*Math.PI);
+        context.fillStyle = "white";
+        context.fill();
+        context.beginPath();
+        context.arc(5, 15, 2.5, 0, Math.PI, false);
+        context.strokeStyle = "white";
+        context.stroke();
+        
+        // restore context angle
+        context.restore();
+        
+    }
 }
--- js/food.js
+++ js/food.js
@@ -1,12 +1,16 @@
-function addFood(cntxt, x, y) {
-    context = cntxt;
-    //http://www.w3schools.com/tags/canvas_globalalpha.asp
-    context.globalAlpha = alpha;
+// food class
+var Food = function (x,y)
+{
+	this.x = x;
+	this.y = y;
 
-    //http://stackoverflow.com/questions/6011378/how-to-add-image-to-canvas
-    food = new Image();
-    food.src = 'images/grape.jpg'; //Image from http://www.grubstreet.com/2014/11/new-york-times-minnesota-grape-recipe.html
-    food.onload = function () {
-        context.drawImage(food, x, y);
+	this.addFood = function (cntxt, x, y) {
+    	context = cntxt;
+    	//http://stackoverflow.com/questions/6011378/how-to-add-image-to-canvas
+    	food = new Image();
+    	food.src = 'images/grape.jpg'; //Image from http://www.grubstreet.com/2014/11/new-york-times-minnesota-grape-recipe.html
+    	food.onload = function () {
+        	context.drawImage(food, x, y);
+    	}
     }
 }
