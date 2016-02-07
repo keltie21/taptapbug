0fc0b6fc17961d0bba96bad0678d50c2890b8a04
Patrick <tundemoran@gmail.com>
2016-02-07 16:42:49 -0500

    commented out food code as is to use blue squares (for ease of debug...)

--- js/food.js
+++ js/food.js
@@ -4,13 +4,17 @@ var Food = function (x,y)
 	this.x = x;
 	this.y = y;
 
-	this.addFood = function (cntxt, x, y) {
+	this.addFood = function (cntxt) {
     	context = cntxt;
+    	context.fillStyle = "blue";
+    	context.fillRect(this.x, this.y, 50, 50);
     	//http://stackoverflow.com/questions/6011378/how-to-add-image-to-canvas
-    	food = new Image();
+    	/*food = new Image();
     	food.src = 'images/grape.jpg'; //Image from http://www.grubstreet.com/2014/11/new-york-times-minnesota-grape-recipe.html
     	food.onload = function () {
-        	context.drawImage(food, x, y);
-    	}
+    		console.log("IN");
+    		console.log(this.x);
+        	context.drawImage(food, food.x, food.y);
+    	}*/
     }
 }
