82c5ff1c4d252e683b5d33caad6fffe38eb11190
SeanWiz <triclopszx@gmail.com>
2016-02-07 23:58:44 -0500

    Debug food
    Switched food back to the blue squares for debugging purposes.

--- js/food.js
+++ js/food.js
@@ -9,12 +9,12 @@ var Food = function (x,y)
 	this.img.src = 'images/grape_clear.png'; //Image from http://www.grubstreet.com/2014/11/new-york-times-minnesota-grape-recipe.html
 
 	this.addFood = function (context) {
-    	/*context = cntxt;
+    	context = cntxt;
     	context.fillStyle = "blue";
-    	context.fillRect(this.x, this.y, 50, 50);*/
+    	context.fillRect(this.x, this.y, 50, 50);
     	
 	    //this.img.src = 'images/grape.jpg'; //Image from http://www.grubstreet.com/2014/11/new-york-times-minnesota-grape-recipe.html
-	    context.drawImage(this.img, this.x, this.y);
+	    //context.drawImage(this.img, this.x, this.y);
 	    this.img.onload = function () {
     		console.log("IN");
     		console.log(this.x);
