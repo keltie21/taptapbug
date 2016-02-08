ffb5068d1d977bc8463f6a492628beb2b66abadb
SeanWiz <triclopszx@gmail.com>
2016-02-08 12:39:41 -0500

    Revert "Added timer functionality"
    This reverts commit 583432e9200e9c2231de5bdacf87134c6d98172b.

--- js/infobar.js
+++ js/infobar.js
@@ -3,13 +3,9 @@
 var timer = 0;
 var score = 0;
 
-
-
 function setInfo(context) {
     timer = 60;
     score = 0;
-    //http://www.w3schools.com/js/js_timing.asp
-    myTimer = setInterval(countdown, 1000);
 }
 
 function addScore(colour) {
@@ -24,11 +20,4 @@ function addScore(colour) {
             score += 5;  //150fps / 60 fs
             break;
     }
-}
-
-function countdown() {
-    timer--;
-    console.log("Time: " + timer);
-    if (timer == 0)
-        clearInterval(myTimer);
 }
