5f35440aadbc651b8ba26a5217230dada702302d
Patrick <tundemoran@gmail.com>
2016-02-08 00:33:17 -0500

    changed food to circle

--- js/food.js
+++ js/food.js
@@ -7,7 +7,11 @@ var Food = function (x,y)
 	this.addFood = function () {
     	
     	context.fillStyle = "blue";
-    	context.fillRect(this.x, this.y, 50, 50);
+    	//context.fillRect(this.x, this.y, 50, 50);
+        context.beginPath();
+        context.arc(this.x + 25, this.y + 25, 25, 0, 2 * Math.PI, false);
+    
+        context.fill();
     	//http://stackoverflow.com/questions/6011378/how-to-add-image-to-canvas
     	/*food = new Image();
     	food.src = 'images/grape.jpg'; //Image from http://www.grubstreet.com/2014/11/new-york-times-minnesota-grape-recipe.html
